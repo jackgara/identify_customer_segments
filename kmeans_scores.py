d55898d8d880cbfcee6f811b8d853caf069b9ffb
jackgara <joaquin.garavaglia@gmail.com>
2021-07-22 22:21:59 +0200

    End FEngineering \ Trained Clusters, Elbow Method for K selection, Data Analysis

---
+++ kmeans_scores.py
@@ -0,0 +1,46 @@
+scores=[55420488.2903,49509011.5373,46400019.9174,44229389.9147,42930363.7901,41699210.9135,41260085.2226,40166090.9033,39421787.5648,]
+n_clusters=[2,4,6,8,10,12,14,16,18,]
+
+'''
+CPU times: user 3 µs, sys: 0 ns, total: 3 µs
+Wall time: 7.87 µs
+Performing K-Means clustering
+Given range min:2, max:14, step:2
+
+Training for n_clusters:  2
+Done... Score:  55420488.2903
+Time elapsed: 40.52 sec.
+
+Training for n_clusters:  4
+Done... Score:  49509011.5373
+Time elapsed: 59.82 sec.
+
+Training for n_clusters:  6
+Done... Score:  46400019.9174
+Time elapsed: 94.24 sec.
+
+Training for n_clusters:  8
+Done... Score:  44229389.9147
+Time elapsed: 96.19 sec.
+
+Training for n_clusters:  10
+Done... Score:  42930363.7901
+Time elapsed: 200.56 sec.
+
+Training for n_clusters:  12
+Done... Score:  41699210.9135
+Time elapsed: 319.94 sec.
+
+Training for n_clusters:  14
+Done... Score:  41260085.2226
+Time elapsed: 369.94 sec.
+
+Training for n_clusters:  16
+Done... Score:  40166090.9033
+Time elapsed: 458.26 sec.
+
+Training for n_clusters:  18
+Done... Score:  39421787.5648
+Time elapsed: 369.97 sec.
+'''
+   
